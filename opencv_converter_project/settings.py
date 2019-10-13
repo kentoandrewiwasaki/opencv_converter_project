03321770692321c2c9f62b64757dd711be1502db
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-10-13 22:58:28 +0900

    Update settings.py

--- opencv_converter_project/settings.py
+++ opencv_converter_project/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 #CSSファイルの保存場所の指定
 STATICFILES_DIRS = [
